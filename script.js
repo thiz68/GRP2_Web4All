285c99c27453846ed44b6023685ebaeb97e10804
Clery <65062914+lecreuseurfou@users.noreply.github.com>
2025-03-04 11:15:27 +0100

    Ajout de la page HTML, du fichier style CSS et du fichier Script JS

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('name').addEventListener('input', function() {this.value = this.value.toUpperCase();});
+    
+function fonction_verification_password(event) {
+    const pass = document.getElementById('password').value;
+    const pass_v = document.getElementById('password_verif').value;
+
+    if (pass != pass_v) {
+        alert("Les mots de passe ne sont pas identiques");
+        event.preventDefault();
+        return false;
+    }
+}
