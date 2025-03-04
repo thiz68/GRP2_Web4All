1f05326dcbe5903a59e3d4e01eda28ba06ef6486
Yanis <119900128+Yukalii@users.noreply.github.com>
2025-03-04 17:45:11 +0100

    Commencement

---
+++ assets/js/profile.js
@@ -0,0 +1,43 @@
+let lastScrollPosition = window.pageYOffset;
+let scrollThreshold = 100;
+
+// ========= Fonction d'Initialisation =========
+// Init chargement page
+document.addEventListener("DOMContentLoaded", function() {
+    // EventListeners
+    setupEventListeners();
+});
+
+// Config EventListeners
+function setupEventListeners() {
+    // Gestion scroll
+    window.addEventListener('scroll', handleScroll);
+}
+
+// ========= Gestion Scroll =========
+function handleScroll() {
+    const currentScrollPosition = window.pageYOffset;
+    const navbar = document.querySelector('.navbar');
+    const backToTop = document.querySelector('.back-to-top');
+
+    // Gestion navbar
+    if (currentScrollPosition > scrollThreshold) {
+        // Si on scroll vers le bas, on cache la navbar
+        if (currentScrollPosition > lastScrollPosition) {
+            navbar.classList.add('hidden');
+        } else {
+            // Si on scroll vers le haut, on affiche la navbar
+            navbar.classList.remove('hidden');
+        }
+    } else {
+        navbar.classList.remove('hidden');
+    }
+
+    // Gestion BackToTop
+    if (currentScrollPosition > scrollThreshold * 2) {
+        backToTop.classList.add('visible');
+    } else {
+        backToTop.classList.remove('visible');
+    }
+    lastScrollPosition = currentScrollPosition;
+}
